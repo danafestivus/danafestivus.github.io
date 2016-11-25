313447ab2d326add1c4f037541e55432f0264779
kennymkchan <kenny.chan@shopify.com>
2016-11-25 00:21:15 -0500

    Re-added images to fit

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function beHappy() {
+  $(".background-wrapper").css({"background": "url('../images/image-2.svg') no-repeat center"});
+}
+
+function beSad() {
+  $(".background-wrapper").css({"background": "url('../images/image-3.svg') no-repeat center"});
+}
+
+function beNormal() {
+  $(".background-wrapper").css({"background": "url('../images/image-1.svg') no-repeat center"});
+}
