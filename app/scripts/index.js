4b4e8fa3305ee18c80c64e32a2d52f6fbfc028a7
kennymkchan <kenny.chan@shopify.com>
2016-11-25 08:41:26 -0500

    Added buttons

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,11 +1,11 @@
-function beHappy() {
+function accept() {
   $(".background-wrapper").css({"background": "url('../images/image-2.svg') no-repeat center"});
 }
 
-function beSad() {
+function decline() {
   $(".background-wrapper").css({"background": "url('../images/image-3.svg') no-repeat center"});
 }
 
-function beNormal() {
+function neutral() {
   $(".background-wrapper").css({"background": "url('../images/image-1.svg') no-repeat center"});
 }
